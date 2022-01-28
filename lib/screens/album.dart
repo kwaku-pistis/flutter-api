064b72144d796ca8bba4c9208d8aaff370ede31b
Pistis <danielpartey.dp@gmail.com>
2022-01-28 12:55:58 +0000

    photos page added

---
+++ lib/screens/album.dart
@@ -0,0 +1,45 @@
+import 'package:api_calls/models/album.dart';
+import 'package:api_calls/providers/providers.dart';
+import 'package:flutter/material.dart';
+
+class AlbumPage extends StatefulWidget {
+  const AlbumPage({ Key? key }) : super(key: key);
+
+  @override
+  _AlbumPageState createState() => _AlbumPageState();
+}
+
+class _AlbumPageState extends State<AlbumPage> {
+  late Future<Album> futureAlbum;
+  final httpService = HttpService();
+  
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = httpService.getAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Album'),
+      ),
+      body: Center(
+        child: FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title, style: Theme.of(context).textTheme.headline4);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/photos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PhotosPage extends StatefulWidget {
+  const PhotosPage({ Key? key }) : super(key: key);
+
+  @override
+  _PhotosPageState createState() => _PhotosPageState();
+}
+
+class _PhotosPageState extends State<PhotosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
